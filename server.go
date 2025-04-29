0273b79329ada2360fe05e057d22b70b2090dbe1
moosewithwolf <moosewithwolf@gmail.com>
2025-04-29 14:33:32 -0400

    hello world message

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server at :4000")
+	http.ListenAndServe(":4000", nil)
+}
